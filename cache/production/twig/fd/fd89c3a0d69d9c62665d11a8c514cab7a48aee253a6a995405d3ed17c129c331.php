e67c2327f0d7ab5b5bcb598ae238996f8f2ba858
vukojevicc <aleksandar.vukojevicc@gmail.com>
2021-03-26 00:23:01 +0100

    fixed problem with including php files in admin panel

--- cache/production/twig/fd/fd89c3a0d69d9c62665d11a8c514cab7a48aee253a6a995405d3ed17c129c331.php
+++ cache/production/twig/fd/fd89c3a0d69d9c62665d11a8c514cab7a48aee253a6a995405d3ed17c129c331.php
@@ -67,76 +67,75 @@ class __TwigTemplate_4287688768e3f4b9ab4fc97156004018e22133c60306cb22ed720be6469
             $this->env->setNamespaceLookUpOrder($previous_look_up_order);
         }
         // line 9
-        echo "        <h1>test</h1>
-\t<div class=\"copyright\">
+        echo "\t<div class=\"copyright\">
 \t\t";
+        // line 10
         // line 11
-        // line 12
         echo "\t\t<p class=\"footer-row\">
 \t\t\t<span class=\"footer-copyright\">";
-        // line 13
+        // line 12
         echo ($context["CREDIT_LINE"] ?? null);
         echo "</span>
 \t\t</p>
 \t\t";
-        // line 15
+        // line 14
         if (($context["TRANSLATION_INFO"] ?? null)) {
-            // line 16
+            // line 15
             echo "\t\t<p class=\"footer-row\">
 \t\t\t<span class=\"footer-copyright\">";
-            // line 17
+            // line 16
             echo ($context["TRANSLATION_INFO"] ?? null);
             echo "</span>
 \t\t</p>
 \t\t";
         }
-        // line 20
+        // line 19
         echo "\t\t";
-        // line 21
+        // line 20
         echo "\t\t<p class=\"footer-row\" role=\"menu\">
 \t\t\t<a class=\"footer-link\" href=\"";
-        // line 22
+        // line 21
         echo ($context["U_PRIVACY"] ?? null);
         echo "\" title=\"";
         echo $this->extensions['phpbb\template\twig\extension']->lang("PRIVACY_LINK");
         echo "\" role=\"menuitem\">
 \t\t\t\t<span class=\"footer-link-text\">";
-        // line 23
+        // line 22
         echo $this->extensions['phpbb\template\twig\extension']->lang("PRIVACY_LINK");
         echo "</span>
 \t\t\t</a>
 \t\t\t|
 \t\t\t<a class=\"footer-link\" href=\"";
-        // line 26
+        // line 25
         echo ($context["U_TERMS_USE"] ?? null);
         echo "\" title=\"";
         echo $this->extensions['phpbb\template\twig\extension']->lang("TERMS_LINK");
         echo "\" role=\"menuitem\">
 \t\t\t\t<span class=\"footer-link-text\">";
-        // line 27
+        // line 26
         echo $this->extensions['phpbb\template\twig\extension']->lang("TERMS_LINK");
         echo "</span>
 \t\t\t</a>
 \t\t</p>
 \t\t";
-        // line 30
+        // line 29
         if (($context["DEBUG_OUTPUT"] ?? null)) {
-            // line 31
+            // line 30
             echo "\t\t<p class=\"footer-row\">
 \t\t\t<span class=\"footer-info\">";
-            // line 32
+            // line 31
             echo ($context["DEBUG_OUTPUT"] ?? null);
             echo "</span>
 \t\t</p>
 \t\t";
         }
-        // line 35
+        // line 34
         echo "\t\t";
         if (($context["U_ACP"] ?? null)) {
-            // line 36
+            // line 35
             echo "\t\t<p class=\"footer-row\">
 \t\t\t<a class=\"footer-link text-strong\" href=\"";
-            // line 37
+            // line 36
             echo ($context["U_ACP"] ?? null);
             echo "\">";
             echo $this->extensions['phpbb\template\twig\extension']->lang("ACP");
@@ -144,11 +143,11 @@ class __TwigTemplate_4287688768e3f4b9ab4fc97156004018e22133c60306cb22ed720be6469
 \t\t</p>
 \t\t";
         }
-        // line 40
+        // line 39
         echo "\t</div>
 
 \t<div id=\"darkenwrapper\" class=\"darkenwrapper\" data-ajax-error-title=\"";
-        // line 42
+        // line 41
         echo $this->extensions['phpbb\template\twig\extension']->lang("AJAX_ERROR_TITLE");
         echo "\" data-ajax-error-text=\"";
         echo $this->extensions['phpbb\template\twig\extension']->lang("AJAX_ERROR_TEXT");
@@ -163,7 +162,7 @@ class __TwigTemplate_4287688768e3f4b9ab4fc97156004018e22133c60306cb22ed720be6469
 \t</div>
 
 \t<div id=\"phpbb_alert\" class=\"phpbb_alert\" data-l-err=\"";
-        // line 46
+        // line 45
         echo $this->extensions['phpbb\template\twig\extension']->lang("ERROR");
         echo "\" data-l-timeout-processing-req=\"";
         echo $this->extensions['phpbb\template\twig\extension']->lang("TIMEOUT_PROCESSING_REQ");
@@ -186,19 +185,19 @@ class __TwigTemplate_4287688768e3f4b9ab4fc97156004018e22133c60306cb22ed720be6469
 <div>
 \t<a id=\"bottom\" class=\"anchor\" accesskey=\"z\"></a>
 \t";
-        // line 64
+        // line 63
         if ( !($context["S_IS_BOT"] ?? null)) {
             echo ($context["RUN_CRON_TASK"] ?? null);
         }
-        // line 65
+        // line 64
         echo "</div>
 
 <script src=\"";
-        // line 67
+        // line 66
         echo ($context["T_JQUERY_LINK"] ?? null);
         echo "\"></script>
 ";
-        // line 68
+        // line 67
         if (($context["S_ALLOW_CDN"] ?? null)) {
             echo "<script>window.jQuery || document.write('\\x3Cscript src=\"";
             echo ($context["T_ASSETS_PATH"] ?? null);
@@ -206,14 +205,14 @@ class __TwigTemplate_4287688768e3f4b9ab4fc97156004018e22133c60306cb22ed720be6469
             echo ($context["T_ASSETS_VERSION"] ?? null);
             echo "\">\\x3C/script>');</script>";
         }
-        // line 69
+        // line 68
         echo "<script src=\"";
         echo ($context["T_ASSETS_PATH"] ?? null);
         echo "/javascript/core.js?assets_version=";
         echo ($context["T_ASSETS_VERSION"] ?? null);
         echo "\"></script>
 ";
-        // line 70
+        // line 69
         $asset_file = "forum_fn.js";
         $asset = new \phpbb\template\asset($asset_file, $this->env->get_path_helper(), $this->env->get_filesystem());
         if (substr($asset_file, 0, 2) !== './' && $asset->is_relative()) {
@@ -227,7 +226,7 @@ class __TwigTemplate_4287688768e3f4b9ab4fc97156004018e22133c60306cb22ed720be6469
         if ($asset->is_relative()) {
             $asset->add_assets_version($this->env->get_phpbb_config()['assets_version']);
         }
-        $this->env->get_assets_bag()->add_script($asset);        // line 71
+        $this->env->get_assets_bag()->add_script($asset);        // line 70
         $asset_file = "ajax.js";
         $asset = new \phpbb\template\asset($asset_file, $this->env->get_path_helper(), $this->env->get_filesystem());
         if (substr($asset_file, 0, 2) !== './' && $asset->is_relative()) {
@@ -241,16 +240,16 @@ class __TwigTemplate_4287688768e3f4b9ab4fc97156004018e22133c60306cb22ed720be6469
         if ($asset->is_relative()) {
             $asset->add_assets_version($this->env->get_phpbb_config()['assets_version']);
         }
-        $this->env->get_assets_bag()->add_script($asset);        // line 72
+        $this->env->get_assets_bag()->add_script($asset);        // line 71
         if (($context["S_ALLOW_CDN"] ?? null)) {
-            // line 73
+            // line 72
             echo "\t<script>
 \t\t(function(\$){
 \t\t\tvar \$fa_cdn = \$('head').find('link[rel=\"stylesheet\"]').first(),
 \t\t\t\t\$span = \$('<span class=\"fa\" style=\"display:none\"></span>').appendTo('body');
 \t\t\tif (\$span.css('fontFamily') !== 'FontAwesome' ) {
 \t\t\t\t\$fa_cdn.after('<link href=\"";
-            // line 78
+            // line 77
             echo ($context["T_ASSETS_PATH"] ?? null);
             echo "/css/font-awesome.min.css\" rel=\"stylesheet\">');
 \t\t\t\t\$fa_cdn.remove();
@@ -260,12 +259,12 @@ class __TwigTemplate_4287688768e3f4b9ab4fc97156004018e22133c60306cb22ed720be6469
 \t</script>
 ";
         }
-        // line 85
+        // line 84
         echo "
 ";
-        // line 86
+        // line 85
         if (($context["S_COOKIE_NOTICE"] ?? null)) {
-            // line 87
+            // line 86
             echo "\t<script src=\"";
             echo ($context["T_ASSETS_PATH"] ?? null);
             echo "/cookieconsent/cookieconsent.min.js?assets_version=";
@@ -286,19 +285,19 @@ class __TwigTemplate_4287688768e3f4b9ab4fc97156004018e22133c60306cb22ed720be6469
 \t\t\t\t\t\"theme\": \"classic\",
 \t\t\t\t\t\"content\": {
 \t\t\t\t\t\t\"message\": \"";
-            // line 102
+            // line 101
             echo twig_escape_filter($this->env, $this->extensions['phpbb\template\twig\extension']->lang("COOKIE_CONSENT_MSG"), "js");
             echo "\",
 \t\t\t\t\t\t\"dismiss\": \"";
-            // line 103
+            // line 102
             echo twig_escape_filter($this->env, $this->extensions['phpbb\template\twig\extension']->lang("COOKIE_CONSENT_OK"), "js");
             echo "\",
 \t\t\t\t\t\t\"link\": \"";
-            // line 104
+            // line 103
             echo twig_escape_filter($this->env, $this->extensions['phpbb\template\twig\extension']->lang("COOKIE_CONSENT_INFO"), "js");
             echo "\",
 \t\t\t\t\t\t\"href\": \"";
-            // line 105
+            // line 104
             echo ($context["UA_PRIVACY"] ?? null);
             echo "\"
 \t\t\t\t\t}
@@ -308,18 +307,18 @@ class __TwigTemplate_4287688768e3f4b9ab4fc97156004018e22133c60306cb22ed720be6469
 \t</script>
 ";
         }
-        // line 112
+        // line 111
         echo "
 ";
-        // line 113
+        // line 112
         $previous_look_up_order = $this->env->getNamespaceLookUpOrder();
         $this->env->setNamespaceLookUpOrder(array('phpbb_viglink', '__main__'));
         $this->env->loadTemplate('@phpbb_viglink/event/overall_footer_after.html')->display($context);
         $this->env->setNamespaceLookUpOrder($previous_look_up_order);
-        // line 114
+        // line 113
         echo "
 ";
-        // line 115
+        // line 114
         if (($context["S_PLUPLOAD"] ?? null)) {
             $location = "plupload.html";
             $namespace = false;
@@ -328,18 +327,18 @@ class __TwigTemplate_4287688768e3f4b9ab4fc97156004018e22133c60306cb22ed720be6469
                 $previous_look_up_order = $this->env->getNamespaceLookUpOrder();
                 $this->env->setNamespaceLookUpOrder(array($namespace, '__main__'));
             }
-            $this->loadTemplate("plupload.html", "overall_footer.html", 115)->display($context);
+            $this->loadTemplate("plupload.html", "overall_footer.html", 114)->display($context);
             if ($namespace) {
                 $this->env->setNamespaceLookUpOrder($previous_look_up_order);
             }
         }
-        // line 116
-        echo twig_get_attribute($this->env, $this->source, ($context["definition"] ?? null), "SCRIPTS", [], "any", false, false, false, 116);
+        // line 115
+        echo twig_get_attribute($this->env, $this->source, ($context["definition"] ?? null), "SCRIPTS", [], "any", false, false, false, 115);
         echo "
 
 ";
+        // line 117
         // line 118
-        // line 119
         echo "
 </body>
 </html>
@@ -358,7 +357,7 @@ class __TwigTemplate_4287688768e3f4b9ab4fc97156004018e22133c60306cb22ed720be6469
 
     public function getDebugInfo()
     {
-        return array (  343 => 119,  342 => 118,  337 => 116,  323 => 115,  320 => 114,  315 => 113,  312 => 112,  302 => 105,  298 => 104,  294 => 103,  290 => 102,  269 => 87,  267 => 86,  264 => 85,  254 => 78,  247 => 73,  245 => 72,  231 => 71,  217 => 70,  210 => 69,  202 => 68,  198 => 67,  194 => 65,  190 => 64,  167 => 46,  152 => 42,  148 => 40,  140 => 37,  137 => 36,  134 => 35,  128 => 32,  125 => 31,  123 => 30,  117 => 27,  111 => 26,  105 => 23,  99 => 22,  96 => 21,  94 => 20,  88 => 17,  85 => 16,  83 => 15,  78 => 13,  75 => 12,  74 => 11,  70 => 9,  58 => 8,  55 => 7,  45 => 5,  43 => 4,  42 => 3,  39 => 2,  37 => 1,);
+        return array (  342 => 118,  341 => 117,  336 => 115,  322 => 114,  319 => 113,  314 => 112,  311 => 111,  301 => 104,  297 => 103,  293 => 102,  289 => 101,  268 => 86,  266 => 85,  263 => 84,  253 => 77,  246 => 72,  244 => 71,  230 => 70,  216 => 69,  209 => 68,  201 => 67,  197 => 66,  193 => 64,  189 => 63,  166 => 45,  151 => 41,  147 => 39,  139 => 36,  136 => 35,  133 => 34,  127 => 31,  124 => 30,  122 => 29,  116 => 26,  110 => 25,  104 => 22,  98 => 21,  95 => 20,  93 => 19,  87 => 16,  84 => 15,  82 => 14,  77 => 12,  74 => 11,  73 => 10,  70 => 9,  58 => 8,  55 => 7,  45 => 5,  43 => 4,  42 => 3,  39 => 2,  37 => 1,);
     }
 
     public function getSourceContext()
